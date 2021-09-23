6cf3e8deffc0614683714de093ba0601a7c8af8c
Hallef <hallef.vieira@hotmail.com>
2021-09-23 00:48:21 -0300

    main: Adjust middlewares

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
-const { v4: uuidv4, validate } = require('uuid');
+const { v4: uuidv4, validate, version } = require('uuid');
 
 const app = express();
 app.use(express.json());
@@ -10,19 +10,65 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers;
+  const user = users.find(user => user.username === username);
+
+  if(!user){
+    return response.status(404).json({error: 'User not found'});
+  }
+
+  request.user = user;
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const {user} = request;
+
+  if((user.pro === false && user.todos.length < 10) || user.pro === true){
+    return next();
+  }
+  return response.status(403).json({error:'Limit todos is exceded'});
+
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers;
+  const {id} = request.params;
+
+  const user = users.find(user => user.username === username);
+  
+  if(!user){
+    return response.status(404).json({error: 'User not found'});
+  }
+  
+  if(validate(id)){
+
+    const todo = user.todos.find(todo => todo.id === id);
+
+    if(!todo){
+
+      return response.status(404).json({error:'Todo not found!'});  
+
+    }
+    request.user = user;
+    request.todo = todo;
+    return next();
+  }
+  
+    return response.status(400).json({error:'todo_id is invalid!'});
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const {id} = request.params;
+  const user = users.find(user => user.id == id);
+
+  if(!user){
+    return response.status(404).json({error:"User not found!"});
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
